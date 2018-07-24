829e8e9198b1f7661348439789a7dda64b55d5fa
sawanoboly <sawanoboriyu@higanworks.com>
2018-07-24 14:05:03 +0900

    0.9.0

--- shifter-artifact-helper.php
+++ shifter-artifact-helper.php
@@ -3,7 +3,7 @@
 Plugin Name: Shifter – Artifact Helper
 Plugin URI: https://github.com/getshifter/shifter-artifact-helper
 Description: Helper tool for building Shifter Artifacts
-Version: 0.8.8
+Version: 0.9.0
 Author: Shifter Team
 Author URI: https://getshifter.io
 License: GPLv2 or later
