ab6af03769d3b22ac68cb3f5e948cfae5691224f
Daniel Olson <emaildano@gmail.com>
2020-05-08 11:50:02 -0400

    Updated Polylang status check

--- include/api.php
+++ include/api.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Get URLs
  *
@@ -7,17 +8,38 @@
  * 
  * @return array|string
  */
-function shifter_get_urls($request_path=null, $rest_request=false)
+
+if (!defined('ABSPATH')) {
+    exit; // don't access directly
+};
+
+function shifter_get_urls($request_path = null, $rest_request = false)
 {
-    if ($rest_request && '/'.ShifterUrlsBase::PATH_404_HTML !== $request_path) {
+    if ($rest_request && '/' . ShifterUrlsBase::PATH_404_HTML !== $request_path) {
         $request_path = trailingslashit($request_path);
     }
 
-    if ((defined('POLYLANG_BASENAME') || (is_plugin_active('polylang/polylang.php'))) &&  function_exists('pll_default_language')) {
+    /**
+     * Check Polylang status
+     * @return boolean
+     */
+    function is_polylang_active()
+    {
+        if (defined('POLYLANG_BASENAME') || defined('POLYLANG_PRO')) {
+            require_once ABSPATH . 'wp-admin/includes/plugin.php';
+            if (is_plugin_active('polylang/polylang.php') && function_exists('pll_default_language')) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    if (is_polylang_active()) {
         // function_exists('pll_default_language')) returns true via rest-api
         // pll_default_language() returns false without any settings
         if (pll_default_language()) {
-            require_once __DIR__.'/class-shifter-urls-polylang.php';
+            require_once __DIR__ . '/class-shifter-urls-polylang.php';
             $shifter_urls = ShifterUrlsPolylang::get_instance();
         } else {
             $shifter_urls = ShifterUrlsBase::get_instance();
@@ -41,20 +63,20 @@ function shifter_get_urls($request_path=null, $rest_request=false)
     $json_data = [];
     $current_url_type = $shifter_urls->current_url_type($request_path, $rest_request);
     switch ($current_url_type) {
-    case ShifterUrlsBase::URL_TOP:
-        $json_data = $shifter_urls->get_urls_all();
-        break;
-    case ShifterUrlsBase::URL_ARCHIVE:
-        $json_data = $shifter_urls->get_urls_archive();
-        break;
-    case ShifterUrlsBase::URL_SINGULAR:
-        $json_data = $shifter_urls->get_urls_singular();
-        break;
-    case ShifterUrlsBase::URL_404:
-        $json_data = $shifter_urls->get_urls_404();
-        break;
-    default:
-        $json_data = $shifter_urls->get_urls();
+        case ShifterUrlsBase::URL_TOP:
+            $json_data = $shifter_urls->get_urls_all();
+            break;
+        case ShifterUrlsBase::URL_ARCHIVE:
+            $json_data = $shifter_urls->get_urls_archive();
+            break;
+        case ShifterUrlsBase::URL_SINGULAR:
+            $json_data = $shifter_urls->get_urls_singular();
+            break;
+        case ShifterUrlsBase::URL_404:
+            $json_data = $shifter_urls->get_urls_404();
+            break;
+        default:
+            $json_data = $shifter_urls->get_urls();
     }
     unset($shifter_urls);
 
